79301c01b15abc686370d6c16392baa363216723
pyroshary <sheharyarghulamrasool@gmail.com>
2024-03-24 02:05:31 +0500

    calcultor file

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//when you run a program computer will generate a number
+// take user input number to guess
+//the  number of user and computer comparison and to show result
+const randomnumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    { name: "userguessnumber",
+        type: "number",
+        message: "please guess a number between 10: " },
+]);
+if (answers.userguessnumber === randomnumber) {
+    console.log("Congartulations you guessed right number.");
+}
+else {
+    console.log("you have guessed the wrong number");
+}
